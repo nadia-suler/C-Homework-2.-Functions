af75d9e6868e9b809d4683ab61cc5f4a26aa80f1
nadia-suler <103899890+nadia-suler@users.noreply.github.com>
2023-03-01 23:29:15 +0100

    Homework 2

---
+++ Homework2.cs
@@ -0,0 +1,53 @@
+//Homework
+    /*
+    //Task 10. Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+    int SecondNumber(int num)
+    {
+      return num / 10 % 10;
+    }
+
+    Console.Write("Введите трехзначное число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    if(num >= 100 && num <= 999)
+    Console.WriteLine($"The second number of {num} is {SecondNumber(num)}");
+    else 
+    Console.WriteLine ("Не трехзначное число :(");
+    */
+
+    //Task 13. Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+   
+   /* 
+    int ThirdNumber(int num)
+    {
+    while (num > 1000)
+      {
+        num = num / 10;
+      }
+      return num = num % 10;
+    }
+
+    Console.Write("Введите число: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+     if(num < 99)
+    {
+      Console.WriteLine("No third number :(");
+    }
+    else
+    {
+      Console.WriteLine(ThirdNumber(num));
+    }
+    */
+
+   /*
+    //Task 15. Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+    bool WeekendNumber(int num)
+    {
+      return num == 6 || num == 7;
+    }
+
+    Console.Write("Введите число дня недели: ");
+    int num = Convert.ToInt32(Console.ReadLine());
+
+    Console.WriteLine(WeekendNumber(num));
+    */
